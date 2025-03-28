d3f7925295c5b4cd765eec8e131e6992c8fb7efe
Adrián Santos <a23311062@uthermosillo.edu.mx>
2025-03-28 08:42:12 -0700

    Init

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::resource('games', App\Http\Controllers\GameController::class);
